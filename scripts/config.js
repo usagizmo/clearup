7fcf4ca8633604442de3d8f129476db15d3397f4
Takafumi Sekine <sekine@rabi.co.jp>
2015-04-02 21:22:56 +0900

    設定ファイルの空行調節

--- scripts/config.js
+++ scripts/config.js
@@ -3,6 +3,7 @@ var config = {};
 
 config.iPhotoLibraryDir = path.join(process.env.HOME, 'Pictures', 'iPhoto Library.photolibrary');
 config.masterDir = path.join(config.iPhotoLibraryDir, 'Masters');
+
 config.appDir = path.join(__dirname, '..');
 config.imageDir = path.join(config.appDir, 'images');
 config.tempDir = path.join(config.appDir, 'temp');
