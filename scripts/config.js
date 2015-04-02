d92e01ec2736812e171adb61bd29926764e71610
Takafumi Sekine <sekine@rabi.co.jp>
2015-04-02 20:25:45 +0900

    サムネイル画像の移動 - get_thumbnails.js 追加

--- scripts/config.js
+++ scripts/config.js
@@ -5,5 +5,6 @@ config.iPhotoLibraryDir = path.join(process.env.HOME, 'Pictures', 'iPhoto Librar
 config.masterDir = path.join(config.iPhotoLibraryDir, 'Masters');
 config.appDir = path.join(__dirname, '..');
 config.imageDir = path.join(config.appDir, 'images');
+config.tempDir = path.join(config.appDir, 'temp');
 
 module.exports = config;
---
+++ scripts/get_thumbnails.js
@@ -0,0 +1,30 @@
+var config = require('./config.js'),
+    fs = require('fs-extra'),
+    glob = require('glob'),
+    path = require('path'),
+    sizeOf = require('image-size');
+
+if (!fs.existsSync(config.tempDir)) fs.mkdirSync(config.tempDir);
+
+glob(config.imageDir + '/**/*', function(err, files) {
+  files.forEach(function(file, i) {
+    var filestat = fs.statSync(file),
+        info = path.parse(file),
+        extname = path.parse(file).ext;
+    if (!filestat.isFile() || !(extname === '.jpg' || extname === '.JPG')) return;
+    var basename = info.base;
+    try {
+      var dimensions = sizeOf(file);
+
+      if ((dimensions.width === 360 && dimensions.height === 270) ||
+          (dimensions.width === 270 && dimensions.height === 360)) {
+
+        var topath = path.join(config.tempDir, basename);
+        fs.rename(file, topath);
+        console.log(i + ': ' + file);
+      }
+    } catch (err) {
+      console.log('Error: ', err);
+    }
+  });
+});
